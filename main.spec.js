37bcfba5b8014b1fdc94ed2f56f508ae5466cb6e
aponoma <siteandstock@gmail.com>
2023-11-08 12:44:00 +0100

    use of this keyword in Jasmine testing

--- main.spec.js
+++ main.spec.js
@@ -12,22 +12,20 @@ describe('main.js', function () {
 
     describe('updateResult()', function () {
 
-        let element;
-
         beforeAll(function () {
-            element = document.createElement('div');
+            const element = document.createElement('div');
             element.setAttribute('id', 'result');
             document.body.appendChild(element);
+            this.element = element;
         });
 
         afterAll(function () {
-            const element = document.getElementById('result');
-            document.body.removeChild(element);
+            document.body.removeChild(this.element);
         });
 
         it('add result to the DOM element', function () {
             updateResult("5");
-            expect(element.innerText).toBe("5");
+            expect(this.element.innerText).toBe("5");
         });
     });
 
